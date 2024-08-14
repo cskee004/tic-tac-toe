59c321ed3ebd6ed2ab0b325661475f671945c598
Chris Skeens <cskee004@odu.edu>
2024-08-14 13:29:02 -0400

    Tested and implemented the start and reset button logic. Added documentation.

--- tictactoe.js
+++ tictactoe.js
@@ -1,20 +1,16 @@
 
-//--------------------------------------------------------------------------->>
 /**
  * The Play class is responsible for controlling the flow of the game. The input method receives coordinates
- * that indicate which of the nine cells was selected. The inputs represent one of the nine cells that make up the play area. 
- * If playControl receives an input, the turn order is first checked, and then the current player id and cell is passed to Gameboard. 
+ * that indicate which of the nine cells was selected. If playControl receives an input, the turn order is first checked, 
+ * and then the current player id and cell is passed to the Gameboard object.  
  * 
- * @return some generic game ending message 
+ * @return The winning players name or if a tie condition was met 
  */
 function Play(xName, oName) {
 
     let board = Gameboard();
-    const playerX = Player(xName, 0);
-    const playerO = Player(oName, 1);
-    let turnOrder = [true, false];
-    let players = [playerX, playerO];
-
+    const turnOrder = [true, false];
+    
     return {
         /**
          * The input method receives (row, col) of the selected cell. The move 
@@ -27,35 +23,36 @@ function Play(xName, oName) {
 
             const r = row;
             const c = col;
-
             const cell = document.querySelector(`[data-rowIndex="${r}"][data-colIndex="${c}"]`);
             
-
             if (turnOrder[0] == true) {
+                console.log(xName)
                 let currentPlayer = board.markCell(row, col, 0);
                 cell.innerHTML = "X";
                 cell.setAttribute('id', "X");
                 
                 if (currentPlayer == 0) {
-                    this.announceWinner(id);
-                    return "Game over";
+                    board = null;
+                    return xName;
                 }
                 
                 turnOrder[0] = false;
                 turnOrder[1] = true;
             }
             else if (turnOrder[1] == true) {
+                console.log(oName)
                 let currentMove = board.markCell(row, col, 1);
                 cell.innerHTML = "O"
                 cell.setAttribute('id', "O");
                 
                 if (currentMove == 1) {
-                    this.announceWinner(id);
-                    // return "Game over";
+                    board = null;
+                    return oName;
+                    
                 }
                 else if(currentMove == -1) {
-                    this.announceTie();
-                    // return "Game over";
+                    board = null;
+                    return "Tie";
                 }
 
                 turnOrder[1] = false;
@@ -65,31 +62,12 @@ function Play(xName, oName) {
             let tempBoard = board.getBoard();
             console.table(tempBoard);
         },
-
-        announceWinner: function(id) {
-            console.log({id})
-            const results = document.querySelector('.game-updates');
-            results.innerHTML = id + " has won the game!"
-
-        },
-
-        annountTie: function() {
-            console.log("Generic tie message")
-            const results = document.querySelector('game-updates');
-            results.innerHTML = "Game ended in a tie"
-        },
-
-        getPlayers: function() {
-            return players;
-        }
-
     }
 }
-
 //--------------------------------------------------------------------------->>
 /**
  * The Gameboard class is responsible for marking the board when a game input is received. The board array represents a 
- * 3x3 matrix. The totalTurns variable is used to check for the game ending tie condition.  
+ * 3x3 matrix. The totalTurns variable is used to check for a game ending tie condition.  
  * @returns The board object
  * @returns The inputs id if the call to markBoard resulted in a winning contion or if the game ended in a tie. 
  * 
@@ -103,7 +81,6 @@ function Gameboard() {
         [-1, -1, -1]
     ];
 
-    // Public API
     return {
         /**
          *  
@@ -114,7 +91,7 @@ function Gameboard() {
         },
         /**
          * The markCell method is responsible for keeping track of what cells have been selected, and
-         * if a tie or winning condtion resulted from the given argument. 
+         * if a tie or winning condtion resulted from that given input. 
          * @param {*} row - The selected cell row
          * @param {*} col - The selected cell column
          * @param {*} id - id of player attached to chosen cell 
@@ -123,8 +100,8 @@ function Gameboard() {
          */
         markCell: function(row, col, id) {
             board[row][col] = id;
-            totalTurns++;
 
+            totalTurns++;
             if (totalTurns == 9) {
                 console.log("Tie")
                 return -1;
@@ -134,43 +111,36 @@ function Gameboard() {
                 if (board[0][i] == id && board[1][i] == id && board[2][i] == id) {
                     return id;
                 }
-
                 if (board[i][0] == id && board[i][1] == id && board[i][2] == id) {
                     return id;
                 }
             }
-            
             if (board[0][0] == id && board[1][1] == id && board[2][2] == id) {
                 return id;
             }
-
             if (board[0][2] == id && board[1][1] == id && board[2][0] == id) {
                 return id;
             }
+            
+
         },
     }
 }
 //--------------------------------------------------------------------------->>
 function Player(name, id) {
     
-    this.name = name;
-    this.id = id;
-
     return {
-        /**
-         * 
-         * @returns 
-         */
+        playerName: name,
+        playerId: id,
+  
         getName: function() {
-            return name;
+            return {playerName};
         }, 
-        /**
-         * 
-         * @returns 
-         */
+
         getId: function() {
-            return id;
+            return {playerId};
         },
     }
 }
+
 
--- tictactoeUI.js
+++ tictactoeUI.js
@@ -1,46 +1,60 @@
+const game = TicTacToe();
 const resetButton = document.querySelector(".reset-button");
 const startButton = document.querySelector(".start-button");
-resetButton.addEventListener("click", TicTacToe, false);
+resetButton.addEventListener("click", game.resetGame, false);
 startButton.addEventListener("click", TicTacToe, false);
-
 /**
  * 
  * 
  */
-function TicTacToe(event) {
-    
-    let p1 = document.querySelector("#p1-name").value;
-    let p2 = document.querySelector("#p2-name").value;
+function TicTacToe() {
+
+    let xName = document.querySelector("#p1-name").value;
+    let oName = document.querySelector("#p2-name").value;
     document.getElementById("p1-name").value = '';
     document.getElementById("p2-name").value = '';
-    console.log(p1, p2);
-
-    let game = Play(p1, p2);
+    
+    let game = Play(xName, oName);
     let turn = 0;
-
+    
+    const gameResults = document.querySelector(".game-updates");
+    gameResults.innerHTML = "";
     const gameButtons = document.querySelectorAll(".cell");
-    gameButtons.forEach(element => {
-        element.addEventListener("click", boardInput, false);
-        //element.setAttribute('id', "on");
+    gameButtons.forEach(cell => {
+        cell.addEventListener("click", boardInput, false);
+        cell.setAttribute('id', "inactive");
+        cell.innerHTML = "";
     });
     //--------------------------------------------------------------------------->>
     /**
-     * This function handles the game boards inputs. A turn variable is used to check if the input was an X or O move. 
-     * After the input is passed to the game logic, that cell is removed from the node list of active cells. 
-     * @param {*} event Represents the current player's selection choice. 
+     * This function handles the users interactions with the gameboard. A turn variable is used to check if it was a player one or player two input. 
+     * The results from the input are returned and then checked for a game ending condition.
+     * @param {*} event Represents what cell was selected. 
      */
     function boardInput(event) {
-    
+        
+        const gameResults = document.querySelector(".game-updates");
         const row = event.target.getAttribute("data-rowIndex");
         const col = event.target.getAttribute("data-colIndex");
         let results = game.playControl(row, col);
+
+        
+        if (results == xName || results == oName) {
+            gameResults.setAttribute('id', "game-over");
+            gameResults.innerHTML = `Congratualtions ${results.toUpperCase()}, you are a winner!`;
+            for (const cell of gameButtons) {
+                cell.removeEventListener("click", boardInput, false);
+                cell.setAttribute('id', "cell-off")
+            }
+        }
+        
         turn++;
-
-        if (results == "Game over") {
-            console.log("Game over signal to turn off gameboard");
-            // Prompt with results
-            // Once clicked, reload page. 
-
+        if (turn == 9) {
+            for (const cell of gameButtons) {
+                cell.removeEventListener("click", boardInput, false);
+            }
+            gameResults.setAttribute('id', "game-over");
+            gameResults.innerHTML = `Tie`;
         }
         
         for (let cell of gameButtons) {
@@ -49,14 +63,18 @@ function TicTacToe(event) {
             }
         }
     }
+
+    return {
+        /**
+         * This method clears any contents from the last game.
+         */
+        resetGame: function() {
+            const gameBoard = document.querySelector(".game-board");
+            gameBoard.setAttribute('id', "deactive");
+            for(const cell of gameButtons) {
+                cell.innerHTML = "";
+                cell.setAttribute('id', "cell-off")
+            }
+        },
+    }
 }
-
-
-
-
-
-
-
-
-
-
